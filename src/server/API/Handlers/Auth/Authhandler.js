3e444fe1840d9baa4d66cdae856e66b6eec0ffe7
toufiq-909 <muheeth312@gmail.com>
2026-04-03 22:27:55 +0530

    feat:enable change password

--- src/server/API/Handlers/Auth/Authhandler.js
+++ src/server/API/Handlers/Auth/Authhandler.js
@@ -7,12 +7,16 @@ export async function login(req,res)
 {
     
     
-    const [result]=await sql `select password from users where username=${req.body.name}`
+    const [result]=await sql `select password,"generatedPassword" from users where username=${req.body.name}`
  
     if(result!=null)
     {
           if(await bcrypt.compare(req.body.password,result.password))
     {
+           if(result.generatedPassword)
+           {
+             return res.status(422).send("Change the password")
+           }
         let token=jwt.sign({username:req.body.username},process.env.Secret);
         return res.status(200).json({token:token})
     } 
@@ -23,3 +27,22 @@ export async function login(req,res)
     
 
 }
+export async function changepassword(req,res)
+{
+const [result]=await sql `select password,"generatedPassword" from users where username=${req.body.name}`
+ 
+    if(result!=null)
+    {
+          if(await bcrypt.compare(req.body.password,result.password))
+    {
+           const updatedHashedPassword=await bcrypt.hash(req.body.newPassword,3);
+           const update=await sql `update users set password=${updatedHashedPassword},"generatedPassword"=false where username=${req.body.name}`
+        let token=jwt.sign({username:req.body.username},process.env.Secret);
+        return res.status(200).json({token:token})
+    } 
+    }
+            return res.sendStatus(401)
+
+
+}
+//try catch
--- src/server/API/Routes/Auth/Auth.js
+++ src/server/API/Routes/Auth/Auth.js
@@ -1,5 +1,6 @@
 import Express from "express"
-import { login } from "../../Handlers/Auth/Authhandler.js";
+import { login,changepassword } from "../../Handlers/Auth/Authhandler.js";
 const Authrouter=Express.Router();
 Authrouter.post("/login",login);
+Authrouter.post("/change",changepassword);
 export default Authrouter;
